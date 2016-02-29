f8d079c3e4bb51d7cb3d72825f5e05a347833851
marabesi <matheus.marabesi@gmail.com>
2016-02-29 20:55:04 -0300

    functions added

--- functions/syntax.php
+++ functions/syntax.php
@@ -1,38 +1,17 @@
 <?php
 
-function sum($a, $b)
+function somar()
 {
+    $argumentos = func_num_args();
+
+    if ($argumentos > 2) {
+        throw new \InvalidArgumentException();
+    }
+
+    $a = func_get_arg(0);
+    $b = func_get_arg(1);
+
     print $a + $b;
 }
 
-sum(10, 20);
-
-
-function transformText($texto)
-{
-    return strtoupper($texto);
-}
-
-print transformText('Hello');
-
-function addElement(array &$collection, $element = null)
-{
-    if ($element !== null)
-    {
-        $collection[] = $element;
-    }
-}
-
-$fruits = [];
-
-addElement($fruits);
-addElement($fruits, 'watermelon');
-
-print_r($fruits);
-
-function exchangeCurrency($coin = 2, $value = 10)
-{
-    return $value * $coin;
-}
-print exchangeCurrency(2);
-
+somar(10);
