fa0b46f73cd1db45b4bb64a84ddeb17707ef6fda
marabesi <matheus.marabesi@gmail.com>
2016-05-08 19:27:56 -0300

    Implemented the feature to see files through menu

--- js/controllers/RepoController.js
+++ js/controllers/RepoController.js
@@ -1,3 +1,11 @@
-app.controller('RepoController', ['$scope', function($scope) {
-    
+app.controller('RepoController', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+    $scope.contents = function() {
+        var base = 'https://api.github.com/repos/marabesi/zcpe/contents/' + $routeParams.data;
+        
+        $http.get(base).then(function (response) {
+            var content = response.data.content;
+            
+            $scope.content = atob(content);
+        })
+    }
 }])
