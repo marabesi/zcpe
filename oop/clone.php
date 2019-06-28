c14696d55017aba78f34a401d10e839987f9a7fd
Matheus Marabesi <matheus.marabesi@gmail.com>
2019-06-28 10:02:55 +0200

    Update clone.php
    updated clone example

--- oop/clone.php
+++ oop/clone.php
@@ -1,50 +1,30 @@
 <?php
 
-//class Name {
-//
-//  public $firstName;
-//  public $lastName;
-//
-//}
-//
-//class Person {
-//
-//  public $name;
-//
-//  public function __construct() {
-//    $this->name = new Name();
-//  }
-//
-//  public function __clone() {
-//   $this->name = clone $this->name;
-//  }
-//}
-//
-//$obj1 = new Person();
-//$obj1->name->firstName = 'Maria';
-//
-//$obj2 = clone $obj1;
-//$obj2->name->firstName = 'Joao';
-//
-//if ($obj1 == $obj2) {
-//  echo 'true';
-//} else {
-//  echo 'false';
-//}
-//
-//echo PHP_EOL;
-//
-//var_export($obj1);
-//var_export($obj2);
-//
-//echo PHP_EOL;
-
-class A {
-    public $name;
+class Name {
+    public $firstName;
 }
 
-$a = new A();
+class Person {
 
-$b = clone $a;
+ public $name;
 
-var_dump(($a == $b));
+ public function __construct() {
+     $this->name = new Name();
+ }
+
+ public function __clone() {
+     $this->name = clone $this->name;
+ }
+}
+
+$obj1 = new Person();
+$obj1->name->firstName = 'Maria';
+
+$obj2 = clone $obj1;
+$obj2->name->firstName = 'Joao';
+
+var_dump($obj1 == $obj2);
+var_dump($obj1->name);
+var_dump($obj2->name);
+
+echo PHP_EOL;
